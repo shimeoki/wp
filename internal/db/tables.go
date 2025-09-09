d00077b3abe0ddee51b0f7cd0bcf372b1dbabc5b
shimeoki <shimeoki@gmail.com>
2025-09-09 22:06:42 +0300

    feat: db tables in go

---
+++ internal/db/tables.go
@@ -0,0 +1,62 @@
+package db
+
+import "time"
+
+type Status struct {
+	ID   int
+	Name string
+}
+
+type Alias struct {
+	ID        int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Tag struct {
+	ID        int
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Source struct {
+	ID        int
+	Name      string
+	Link      *string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Wallpaper struct {
+	ID        int
+	Hash      string
+	Extension string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type WallpaperAlias struct {
+	WallpaperID int
+	AliasID     int
+}
+
+type WallpaperTag struct {
+	WallpaperID int
+	TagID       int
+}
+
+type WallpaperSource struct {
+	WallpaperID int
+	SourceID    int
+}
+
+type Queue struct {
+	ID          int
+	WallpaperID int
+	StatusID    int
+	Priority    int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
