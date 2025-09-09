bc5f4e978860942f370a452e2bb7357fea660a57
shimeoki <shimeoki@gmail.com>
2025-09-09 13:18:16 +0300

    feat: alias table

--- db.sql
+++ db.sql
@@ -2,3 +2,10 @@ create table status (
     id     integer primary key
     , name text not null unique
 );
+
+create table alias (
+    id           integer primary key
+    , name       text not null unique
+    , created_at text not null default current_timestamp
+    , updated_at text not null default current_timestamp
+);
