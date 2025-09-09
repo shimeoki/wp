f78de3749acf35313b6d9465e07341fddc031e3b
shimeoki <shimeoki@gmail.com>
2025-09-09 13:18:39 +0300

    style: use int instead of integer

--- db.sql
+++ db.sql
@@ -1,10 +1,10 @@
 create table status (
-    id     integer primary key
+    id     int  primary key
     , name text not null unique
 );
 
 create table alias (
-    id           integer primary key
+    id           int  primary key
     , name       text not null unique
     , created_at text not null default current_timestamp
     , updated_at text not null default current_timestamp
