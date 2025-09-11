d0749886b8ea2d2682c24f35b97ccafe874d6b21
shimeoki <shimeoki@gmail.com>
2025-09-11 20:06:53 +0300

    feat: use variadic functions in WallpaperRepo

--- internal/db/wallpaper.go
+++ internal/db/wallpaper.go
@@ -30,19 +30,19 @@ type WallpaperSource struct {
 
 type WallpaperRepo interface {
 	GetAll(ctx context.Context) ([]*Wallpaper, error)
-	GetByID(ctx context.Context, id int) (*Wallpaper, error)
-	GetByHash(ctx context.Context, hash string) (*Wallpaper, error)
+	GetByID(ctx context.Context, ids ...int) ([]*Wallpaper, error)
+	GetByHash(ctx context.Context, hashes ...string) ([]*Wallpaper, error)
 
-	Create(ctx context.Context, w *Wallpaper) error
-	Update(ctx context.Context, w *Wallpaper) error
-	Delete(ctx context.Context, id int) error
+	Create(ctx context.Context, ws ...*Wallpaper) error
+	Update(ctx context.Context, ws ...*Wallpaper) error
+	Delete(ctx context.Context, ids ...int) error
 
-	AddAlias(ctx context.Context, ids *WallpaperAlias) error
-	RemoveAlias(ctx context.Context, ids *WallpaperAlias) error
+	AddAlias(ctx context.Context, joins ...*WallpaperAlias) error
+	RemoveAlias(ctx context.Context, joins ...*WallpaperAlias) error
 
-	AddTag(ctx context.Context, ids *WallpaperTag) error
-	RemoveTag(ctx context.Context, ids *WallpaperTag) error
+	AddTag(ctx context.Context, joins ...*WallpaperTag) error
+	RemoveTag(ctx context.Context, joins ...*WallpaperTag) error
 
-	AddSource(ctx context.Context, ids *WallpaperSource) error
-	RemoveSource(ctx context.Context, ids *WallpaperSource) error
+	AddSource(ctx context.Context, joins ...*WallpaperSource) error
+	RemoveSource(ctx context.Context, joins ...*WallpaperSource) error
 }
