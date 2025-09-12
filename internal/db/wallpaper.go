bcb1468359fdcb7bfd92eb254a3d8c4cf7e6b821
shimeoki <shimeoki@gmail.com>
2025-09-12 16:09:01 +0300

    feat!: match wallpaper.go to status.go

--- internal/db/wallpaper.go
+++ internal/db/wallpaper.go
@@ -6,43 +6,43 @@ import (
 )
 
 type Wallpaper struct {
-	ID        int
-	Hash      string
+	ID        int64  // primary key
+	Hash      string // unique
 	Extension string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
 type WallpaperAlias struct {
-	WallpaperID int
-	AliasID     int
+	WallpaperID int64 // foreign key
+	AliasID     int64 // foreign key
 }
 
 type WallpaperTag struct {
-	WallpaperID int
-	TagID       int
+	WallpaperID int64 // foreign key
+	TagID       int64 // foreign key
 }
 
 type WallpaperSource struct {
-	WallpaperID int
-	SourceID    int
+	WallpaperID int64 // foreign key
+	SourceID    int64 // foreign key
 }
 
 type WallpaperRepo interface {
 	GetAll(ctx context.Context) ([]*Wallpaper, error)
-	GetByID(ctx context.Context, ids ...int) ([]*Wallpaper, error)
-	GetByHash(ctx context.Context, hashes ...string) ([]*Wallpaper, error)
+	GetByID(ctx context.Context, id int64) (*Wallpaper, error)
+	GetByHash(ctx context.Context, hash string) (*Wallpaper, error)
 
-	Create(ctx context.Context, ws ...*Wallpaper) error
-	Update(ctx context.Context, ws ...*Wallpaper) error
-	Delete(ctx context.Context, ids ...int) error
+	Create(ctx context.Context, w *Wallpaper) error
+	Update(ctx context.Context, w *Wallpaper) error
+	Delete(ctx context.Context, id int64) error
 
-	AddAlias(ctx context.Context, joins ...*WallpaperAlias) error
-	RemoveAlias(ctx context.Context, joins ...*WallpaperAlias) error
+	AddAlias(ctx context.Context, join *WallpaperAlias) error
+	RemoveAlias(ctx context.Context, join *WallpaperAlias) error
 
-	AddTag(ctx context.Context, joins ...*WallpaperTag) error
-	RemoveTag(ctx context.Context, joins ...*WallpaperTag) error
+	AddTag(ctx context.Context, join *WallpaperTag) error
+	RemoveTag(ctx context.Context, join *WallpaperTag) error
 
-	AddSource(ctx context.Context, joins ...*WallpaperSource) error
-	RemoveSource(ctx context.Context, joins ...*WallpaperSource) error
+	AddSource(ctx context.Context, join *WallpaperSource) error
+	RemoveSource(ctx context.Context, join *WallpaperSource) error
 }
