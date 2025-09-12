b24e7b17121171fe5ca65e9ad7dac1c25d6fa282
shimeoki <shimeoki@gmail.com>
2025-09-12 16:38:02 +0300

    feat: sqliteWallpaperRepo

--- internal/db/wallpaper.go
+++ internal/db/wallpaper.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"time"
 )
 
@@ -45,4 +46,198 @@ type WallpaperRepo interface {
 
 	AddSource(ctx context.Context, join *WallpaperSource) error
 	RemoveSource(ctx context.Context, join *WallpaperSource) error
+}
+
+type sqliteWallpaperRepo struct {
+	db *sql.DB
+}
+
+func (r *sqliteWallpaperRepo) GetAll(
+	ctx context.Context,
+) ([]*Wallpaper, error) {
+	sql := "select id, hash, extension, created_at, updated_at from wallpaper"
+
+	rows, err := r.db.QueryContext(ctx, sql)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var ws []*Wallpaper
+
+	for rows.Next() {
+		var w Wallpaper
+
+		err := rows.Scan(
+			&w.ID,
+			&w.Hash,
+			&w.Extension,
+			&w.CreatedAt,
+			&w.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		ws = append(ws, &w)
+	}
+
+	return ws, rows.Err()
+}
+
+func (r *sqliteWallpaperRepo) GetByID(
+	ctx context.Context,
+	id int64,
+) (*Wallpaper, error) {
+	sql := `
+		select
+			id,
+			, hash
+			, extension
+			, created_at
+			, updated_at
+		from wallpaper where id = ?`
+
+	var w Wallpaper
+	err := r.db.QueryRowContext(ctx, sql, id).
+		Scan(
+			&w.ID,
+			&w.Hash,
+			&w.Extension,
+			&w.CreatedAt,
+			&w.UpdatedAt,
+		)
+
+	return &w, err
+}
+
+func (r *sqliteWallpaperRepo) GetByHash(
+	ctx context.Context,
+	hash string,
+) (*Wallpaper, error) {
+	sql := `
+		select
+			id,
+			, hash
+			, extension
+			, created_at
+			, updated_at
+		from wallpaper where hash = ?`
+
+	var w Wallpaper
+	err := r.db.QueryRowContext(ctx, sql, hash).
+		Scan(
+			&w.ID,
+			&w.Hash,
+			&w.Extension,
+			&w.CreatedAt,
+			&w.UpdatedAt,
+		)
+
+	return &w, err
+}
+
+func (r *sqliteWallpaperRepo) Create(ctx context.Context, w *Wallpaper) error {
+	sql := "insert into wallpaper(hash, extension) values (?, ?)"
+
+	result, err := r.db.ExecContext(ctx, sql, w.Hash, w.Extension)
+	if err != nil {
+		return err
+	}
+
+	w.ID, err = result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *sqliteWallpaperRepo) Update(ctx context.Context, w *Wallpaper) error {
+	sql := "update wallpaper set hash = ?, extension = ? where id = ?"
+
+	_, err := r.db.ExecContext(ctx, sql, w.Hash, w.Extension, w.ID)
+
+	return err
+}
+
+func (r *sqliteWallpaperRepo) Delete(ctx context.Context, id int64) error {
+	sql := "delete from wallpaper where id = ?"
+
+	_, err := r.db.ExecContext(ctx, sql, id)
+
+	return err
+}
+
+func (r *sqliteWallpaperRepo) AddAlias(
+	ctx context.Context,
+	join *WallpaperAlias,
+) error {
+	sql := `
+		insert into wallpaper_alias(wallpaper_id, alias_id)
+		values(?, ?)`
+
+	_, err := r.db.ExecContext(ctx, sql, join.WallpaperID, join.AliasID)
+
+	return err
+}
+
+func (r *sqliteWallpaperRepo) RemoveAlias(
+	ctx context.Context,
+	join *WallpaperAlias,
+) error {
+	sql := "delete from wallpaper_alias where wallpaper_id = ?, alias_id = ?"
+
+	_, err := r.db.ExecContext(ctx, sql, join.WallpaperID, join.AliasID)
+
+	return err
+}
+
+func (r *sqliteWallpaperRepo) AddTag(
+	ctx context.Context,
+	join *WallpaperTag,
+) error {
+	sql := `
+		insert into wallpaper_tag(wallpaper_id, tag_id)
+		values(?, ?)`
+
+	_, err := r.db.ExecContext(ctx, sql, join.WallpaperID, join.TagID)
+
+	return err
+}
+
+func (r *sqliteWallpaperRepo) RemoveTag(
+	ctx context.Context,
+	join *WallpaperTag,
+) error {
+	sql := "delete from wallpaper_tag where wallpaper_id = ?, tag_id = ?"
+
+	_, err := r.db.ExecContext(ctx, sql, join.WallpaperID, join.TagID)
+
+	return err
+}
+
+func (r *sqliteWallpaperRepo) AddSource(
+	ctx context.Context,
+	join *WallpaperSource,
+) error {
+	sql := `
+		insert into wallpaper_source(wallpaper_id, source_id)
+		values(?, ?)`
+
+	_, err := r.db.ExecContext(ctx, sql, join.WallpaperID, join.SourceID)
+
+	return err
+}
+
+func (r *sqliteWallpaperRepo) RemoveSource(
+	ctx context.Context,
+	join *WallpaperSource,
+) error {
+	sql := "delete from wallpaper_source where wallpaper_id = ?, source_id = ?"
+
+	_, err := r.db.ExecContext(ctx, sql, join.WallpaperID, join.SourceID)
+
+	return err
 }
