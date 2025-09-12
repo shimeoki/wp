c7471cb1ddd7b626c3b766f80b1cd528e8f6b9fb
shimeoki <shimeoki@gmail.com>
2025-09-12 14:11:34 +0300

    feat: sqliteTagRepo

--- internal/db/tag.go
+++ internal/db/tag.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"time"
 )
 
@@ -20,4 +21,88 @@ type TagRepo interface {
 	Create(ctx context.Context, t *Tag) error
 	Update(ctx context.Context, t *Tag) error
 	Delete(ctx context.Context, id int64) error
+}
+
+type sqliteTagRepo struct {
+	db *sql.DB
+}
+
+func (r *sqliteTagRepo) GetAll(ctx context.Context) ([]*Tag, error) {
+	sql := "select id, name, created_at, updated_at from tag"
+
+	rows, err := r.db.QueryContext(ctx, sql)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var ts []*Tag
+
+	for rows.Next() {
+		var t Tag
+
+		err := rows.Scan(&t.ID, &t.Name, &t.CreatedAt, &t.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		ts = append(ts, &t)
+	}
+
+	return ts, rows.Err()
+}
+
+func (r *sqliteTagRepo) GetByID(ctx context.Context, id int64) (*Tag, error) {
+	sql := "select id, name, created_at, updated_at from tag where id = ?"
+
+	var t Tag
+	err := r.db.QueryRowContext(ctx, sql, id).
+		Scan(&t.ID, &t.Name, &t.CreatedAt, &t.UpdatedAt)
+
+	return &t, err
+}
+
+func (r *sqliteTagRepo) GetByName(
+	ctx context.Context,
+	name string,
+) (*Tag, error) {
+	sql := "select id, name, created_at, updated_at from tag where name = ?"
+
+	var t Tag
+	err := r.db.QueryRowContext(ctx, sql, name).
+		Scan(&t.ID, &t.Name, &t.CreatedAt, &t.UpdatedAt)
+
+	return &t, err
+}
+
+func (r *sqliteTagRepo) Create(ctx context.Context, t *Tag) error {
+	sql := "insert into tag(name) values (?)"
+
+	result, err := r.db.ExecContext(ctx, sql, t.Name)
+	if err != nil {
+		return err
+	}
+
+	t.ID, err = result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *sqliteTagRepo) Update(ctx context.Context, t *Tag) error {
+	sql := "update tag set name = ? where id = ?"
+
+	_, err := r.db.ExecContext(ctx, sql, t.Name, t.ID)
+
+	return err
+}
+
+func (r *sqliteTagRepo) Delete(ctx context.Context, id int64) error {
+	sql := "delete from tag where id = ?"
+
+	_, err := r.db.ExecContext(ctx, sql, id)
+
+	return err
 }
