74b5aeaf4d183f8f4de564912dfddaa5eb0ab40c
shimeoki <shimeoki@gmail.com>
2025-09-12 14:02:28 +0300

    feat!: change tag.to to match status.go

--- internal/db/tag.go
+++ internal/db/tag.go
@@ -6,18 +6,18 @@ import (
 )
 
 type Tag struct {
-	ID        int
-	Name      string
+	ID        int64  // primary key
+	Name      string // unique
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
 type TagRepo interface {
 	GetAll(ctx context.Context) ([]*Tag, error)
-	GetByID(ctx context.Context, ids ...int) ([]*Tag, error)
-	GetByName(ctx context.Context, names ...string) ([]*Tag, error)
+	GetByID(ctx context.Context, id int64) (*Tag, error)
+	GetByName(ctx context.Context, name string) (*Tag, error)
 
-	Create(ctx context.Context, ts ...*Tag) error
-	Update(ctx context.Context, ts ...*Tag) error
-	Delete(ctx context.Context, ids ...int) error
+	Create(ctx context.Context, t *Tag) error
+	Update(ctx context.Context, t *Tag) error
+	Delete(ctx context.Context, id int64) error
 }
