21a5b38def5ade4fe566bcb2ed901a44c5ef0483
shimeoki <shimeoki@gmail.com>
2025-09-20 09:19:52 +0300

    feat(store): hash.go

---
+++ internal/store/hash.go
@@ -0,0 +1,40 @@
+package store
+
+import (
+	"crypto/sha256"
+	"hash"
+	"io"
+	"regexp"
+)
+
+type Hasher interface {
+	Compute(r io.Reader) (hash string, err error)
+	Valid(hash string) bool
+}
+
+type SHA256Hasher struct {
+	hsh hash.Hash
+}
+
+func NewSHA256Hasher() *SHA256Hasher {
+	return &SHA256Hasher{hsh: sha256.New()}
+}
+
+func (h *SHA256Hasher) Compute(r io.Reader) (string, error) {
+	h.hsh.Reset()
+
+	if _, err := io.Copy(h.hsh, r); err != nil {
+		return "", err
+	}
+
+	return string(h.hsh.Sum(nil)), nil
+}
+
+func (h *SHA256Hasher) Valid(hash string) bool {
+	match, err := regexp.MatchString("^[a-fA-F0-9]{64}$", hash)
+	if err != nil {
+		return false
+	}
+
+	return match
+}
