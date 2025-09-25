df1b69171a4992bca010f805dfbe0bb326d083f3
shimeoki <shimeoki@gmail.com>
2025-09-25 21:29:54 +0300

    feat!: config from flags

--- cmd/root.go
+++ cmd/root.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/shimeoki/wp/internal/config"
 	"github.com/spf13/cobra"
 )
 
+var cfg config.Config
+
 var rootCmd = &cobra.Command{
 	Use: "wp",
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(
+		&cfg.DB.DataSourceName, "db-dsn",
+		"", "database data source name")
 }
 
 func Execute() {
