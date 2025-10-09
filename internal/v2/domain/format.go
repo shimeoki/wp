8cfb4b8fe641288f9f733aba96c95ab786d65f3c
shimeoki <shimeoki@gmail.com>
2025-10-09 20:46:56 +0300

    feat(domain): ParseFormat

--- internal/v2/domain/format.go
+++ internal/v2/domain/format.go
@@ -1,6 +1,9 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 type Format string
 
@@ -10,3 +13,15 @@ const (
 )
 
 var InvalidFormat = errors.New("invalid format")
+
+func ParseFormat(extension string) (Format, error) {
+	switch strings.ToLower(extension) {
+	case "jpeg":
+	case "jpg":
+		return JPEG, nil
+	case "png":
+		return PNG, nil
+	}
+
+	return "", InvalidFormat
+}
