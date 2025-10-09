c4c079a49a20274b116d81537af8a84ac784ff7c
shimeoki <shimeoki@gmail.com>
2025-10-09 21:18:11 +0300

    refactor(app): rename remove source cmd

--- internal/v2/app/source_remove.go
+++ internal/v2/app/source_remove.go
@@ -7,43 +7,43 @@ import (
 	"github.com/shimeoki/wp/internal/v2/domain"
 )
 
-type RemoveSourceCommand struct {
+type RemoveSourceHandler struct {
 	wallpapers domain.WallpaperRepo
 	sources    domain.SourceRepo
 }
 
-func NewRemoveSourceCommand(
+func NewRemoveSourceHandler(
 	wallpapers domain.WallpaperRepo,
 	sources domain.SourceRepo,
-) *RemoveSourceCommand {
-	return &RemoveSourceCommand{
+) *RemoveSourceHandler {
+	return &RemoveSourceHandler{
 		wallpapers: wallpapers,
 		sources:    sources,
 	}
 }
 
-type RemoveSourceData struct {
+type RemoveSourceCommand struct {
 	WallpaperHash string
 	SourceID      string
 }
 
 type RemoveSourceResult struct{}
 
-func (cmd *RemoveSourceCommand) Execute(
+func (h *RemoveSourceHandler) Handle(
 	ctx Ctx,
-	data *RemoveSourceData,
+	cmd *RemoveSourceCommand,
 ) (*RemoveSourceResult, error) {
-	w, _ := cmd.wallpapers.ByHash(ctx, domain.Hash(data.WallpaperHash))
+	w, _ := h.wallpapers.ByHash(ctx, domain.Hash(cmd.WallpaperHash))
 	if w == nil {
 		return nil, errors.New("wallpaper not found")
 	}
 
-	id, err := uuid.Parse(data.SourceID)
+	id, err := uuid.Parse(cmd.SourceID)
 	if err != nil {
 		return nil, err
 	}
 
-	source, _ := cmd.sources.ByID(ctx, domain.ID(id))
+	source, _ := h.sources.ByID(ctx, domain.ID(id))
 	if source == nil {
 		return nil, errors.New("source not found")
 	}
@@ -56,7 +56,7 @@ func (cmd *RemoveSourceCommand) Execute(
 		return nil, err
 	}
 
-	if err := cmd.wallpapers.Save(ctx, w); err != nil {
+	if err := h.wallpapers.Save(ctx, w); err != nil {
 		return nil, err
 	}
 
