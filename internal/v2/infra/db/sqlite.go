a16b20bad02d56100ec27e0e7a6e0e64690bf2c4
shimeoki <shimeoki@gmail.com>
2025-10-11 16:57:15 +0300

    feat(db): sqlite connection

---
+++ internal/v2/infra/db/sqlite.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	_ "embed"
+	"time"
+
+	"github.com/shimeoki/wp/internal/config"
+	"github.com/shimeoki/wp/internal/v2/domain"
+)
+
+type DB interface {
+	ExecContext(ctx domain.Ctx, query string, args ...any) (sql.Result, error)
+	QueryContext(ctx domain.Ctx, query string, args ...any) (*sql.Rows, error)
+	QueryRowContext(ctx domain.Ctx, query string, args ...any) *sql.Row
+}
+
+//go:embed sqlite.sql
+var sqliteScheme string
+
+func OpenSQLiteDB(ctx domain.Ctx, cfg *config.DB) (*sql.DB, error) {
+	db, err := sql.Open("sqlite", cfg.DataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ping(db); err != nil {
+		return nil, err
+	}
+
+	if _, err := db.ExecContext(ctx, sqliteScheme); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func ping(db *sql.DB) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	return db.PingContext(ctx)
+}
