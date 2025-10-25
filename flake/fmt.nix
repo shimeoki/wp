cee282a60c86f9489d7943fb285ceba4a9887f43
shimeoki <shimeoki@gmail.com>
2025-10-25 18:46:20 +0300

    refactor(flake): separate file for treefmt cfg

--- flake/fmt.nix
+++ flake/fmt.nix
@@ -5,32 +5,6 @@
     ];
 
     perSystem = {
-        treefmt.programs = {
-            # keep-sorted start block=yes newline_separated=yes
-            deno = {
-                enable = true;
-                includes = [
-                    "deno.json"
-                    "README.md"
-                ];
-            };
-
-            golines = {
-                enable = true;
-                maxLength = 80;
-                tabLength = 4;
-            };
-
-            keep-sorted = {
-                enable = true;
-            };
-
-            nixfmt = {
-                enable = true;
-                width = 80;
-                indent = 4;
-            };
-            # keep-sorted end
-        };
+        treefmt = import "${inputs.self}/treefmt.nix";
     };
 }
