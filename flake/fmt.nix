f9059928fd07396e51e660f7465b3f90fb26b592
shimeoki <shimeoki@gmail.com>
2025-10-25 15:59:26 +0300

    refactor(flake): use nixfmt indent

--- flake/fmt.nix
+++ flake/fmt.nix
@@ -5,30 +5,24 @@
     ];
 
     perSystem = {
-        treefmt = {
-            programs = {
-                # keep-sorted start block=yes newline_separated=yes
-                golines = {
-                    enable = true;
-                    maxLength = 80;
-                    tabLength = 4;
-                };
-
-                keep-sorted = {
-                    enable = true;
-                };
-
-                nixfmt = {
-                    enable = true;
-                    width = 80;
-                };
-                # keep-sorted end
+        treefmt.programs = {
+            # keep-sorted start block=yes newline_separated=yes
+            golines = {
+                enable = true;
+                maxLength = 80;
+                tabLength = 4;
             };
 
-            settings.formatter = {
-                # TODO: use indent option after numtide/treefmt-nix#416
-                nixfmt.options = [ "--indent=4" ];
+            keep-sorted = {
+                enable = true;
             };
+
+            nixfmt = {
+                enable = true;
+                width = 80;
+                indent = 4;
+            };
+            # keep-sorted end
         };
     };
 }
