d9bf02565888ab9e7d329a8c3e77c11874fd7792
shimeoki <shimeoki@gmail.com>
2025-10-28 20:23:53 +0300

    feat(config): add remaining handlers

--- internal/config/handlers.go
+++ internal/config/handlers.go
@@ -3,83 +3,153 @@ package config
 import "github.com/shimeoki/wp/internal/app"
 
 type Handlers struct {
-	CreateWallpaper *app.CreateWallpaperHandler
-	DeleteWallpaper *app.DeleteWallpaperHandler
-	FindWallpaper   *app.FindWallpaperHandler
-	ShowWallpaper   *app.ShowWallpaperHandler
+	AddSource    *app.AddSourceHandler
+	CreateSource *app.CreateSourceHandler
+	DeleteSource *app.DeleteSourceHandler
+	ListSources  *app.ListSourcesHandler
+	RemoveSource *app.RemoveSourceHandler
 
 	AddTag    *app.AddTagHandler
 	CreateTag *app.CreateTagHandler
 	DeleteTag *app.DeleteTagHandler
 	ListTags  *app.ListTagsHandler
+	RemoveTag *app.RemoveTagHandler
+	RenameTag *app.RenameTagHandler
+
+	CreateWallpaper *app.CreateWallpaperHandler
+	DeleteWallpaper *app.DeleteWallpaperHandler
+	FindWallpaper   *app.FindWallpaperHandler
+	ShowWallpaper   *app.ShowWallpaperHandler
 }
 
-// TODO: very ugly, but i don't know is this possible to DRY
+// NOTE: it looks very ugly, but i don't think it's possible to DRY.
+// if you are going to make a generic wrapper function, then the compiler
+// errors in the function body, because it doesn't know will the *Provider
+// satisfy the specific provider or not
 
 func NewHandlers(w app.Worker[*Provider]) *Handlers {
 	return &Handlers{
-		CreateWallpaper: app.NewCreateWallpaperHandler(
-			app.WorkerFunc[app.CreateWallpaperProvider](
-				func(ctx app.Ctx, j app.Job[app.CreateWallpaperProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+		AddSource:    app.NewAddSourceHandler(addSource(w)),
+		CreateSource: app.NewCreateSourceHandler(createSource(w)),
+		DeleteSource: app.NewDeleteSourceHandler(deleteSource(w)),
+		ListSources:  app.NewListSourcesHandler(listSources(w)),
+		RemoveSource: app.NewRemoveSourceHandler(removeSource(w)),
 
-		DeleteWallpaper: app.NewDeleteWallpaperHandler(
-			app.WorkerFunc[app.DeleteWallpaperProvider](
-				func(ctx app.Ctx, j app.Job[app.DeleteWallpaperProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+		AddTag:    app.NewAddTagHandler(addTag(w)),
+		CreateTag: app.NewCreateTagHandler(createTag(w)),
+		DeleteTag: app.NewDeleteTagHandler(deleteTag(w)),
+		ListTags:  app.NewListTagsHandler(listTags(w)),
+		RemoveTag: app.NewRemoveTagHandler(removeTag(w)),
+		RenameTag: app.NewRenameTagHandler(renameTag(w)),
 
-		FindWallpaper: app.NewFindWallpaperHandler(
-			app.WorkerFunc[app.FindWallpaperProvider](
-				func(ctx app.Ctx, j app.Job[app.FindWallpaperProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+		CreateWallpaper: app.NewCreateWallpaperHandler(createWallpaper(w)),
+		DeleteWallpaper: app.NewDeleteWallpaperHandler(deleteWallpaper(w)),
+		FindWallpaper:   app.NewFindWallpaperHandler(findWallpaper(w)),
+		ShowWallpaper:   app.NewShowWallpaperHandler(showWallpaper(w)),
+	}
+}
 
-		ShowWallpaper: app.NewShowWallpaperHandler(
-			app.WorkerFunc[app.ShowWallpaperProvider](
-				func(ctx app.Ctx, j app.Job[app.ShowWallpaperProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+func addSource(w app.Worker[*Provider]) app.WorkerFunc[app.AddSourceProvider] {
+	return func(ctx app.Ctx, j app.Job[app.AddSourceProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
 
-		AddTag: app.NewAddTagHandler(
-			app.WorkerFunc[app.AddTagProvider](
-				func(ctx app.Ctx, j app.Job[app.AddTagProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+func createSource(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.CreateSourceProvider] {
+	return func(ctx app.Ctx, j app.Job[app.CreateSourceProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
 
-		CreateTag: app.NewCreateTagHandler(
-			app.WorkerFunc[app.CreateTagProvider](
-				func(ctx app.Ctx, j app.Job[app.CreateTagProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+func deleteSource(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.DeleteSourceProvider] {
+	return func(ctx app.Ctx, j app.Job[app.DeleteSourceProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
 
-		DeleteTag: app.NewDeleteTagHandler(
-			app.WorkerFunc[app.DeleteTagProvider](
-				func(ctx app.Ctx, j app.Job[app.DeleteTagProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+func listSources(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.ListSourcesProvider] {
+	return func(ctx app.Ctx, j app.Job[app.ListSourcesProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
 
-		ListTags: app.NewListTagsHandler(
-			app.WorkerFunc[app.ListTagsProvider](
-				func(ctx app.Ctx, j app.Job[app.ListTagsProvider]) error {
-					return w.Work(ctx, func(p *Provider) error { return j(p) })
-				},
-			),
-		),
+func removeSource(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.RemoveSourceProvider] {
+	return func(ctx app.Ctx, j app.Job[app.RemoveSourceProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+func addTag(w app.Worker[*Provider]) app.WorkerFunc[app.AddTagProvider] {
+	return func(ctx app.Ctx, j app.Job[app.AddTagProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func createTag(w app.Worker[*Provider]) app.WorkerFunc[app.CreateTagProvider] {
+	return func(ctx app.Ctx, j app.Job[app.CreateTagProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func deleteTag(w app.Worker[*Provider]) app.WorkerFunc[app.DeleteTagProvider] {
+	return func(ctx app.Ctx, j app.Job[app.DeleteTagProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func listTags(w app.Worker[*Provider]) app.WorkerFunc[app.ListTagsProvider] {
+	return func(ctx app.Ctx, j app.Job[app.ListTagsProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func removeTag(w app.Worker[*Provider]) app.WorkerFunc[app.RemoveTagProvider] {
+	return func(ctx app.Ctx, j app.Job[app.RemoveTagProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func renameTag(w app.Worker[*Provider]) app.WorkerFunc[app.RenameTagProvider] {
+	return func(ctx app.Ctx, j app.Job[app.RenameTagProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func createWallpaper(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.CreateWallpaperProvider] {
+	return func(ctx app.Ctx, j app.Job[app.CreateWallpaperProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func deleteWallpaper(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.DeleteWallpaperProvider] {
+	return func(ctx app.Ctx, j app.Job[app.DeleteWallpaperProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func findWallpaper(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.FindWallpaperProvider] {
+	return func(ctx app.Ctx, j app.Job[app.FindWallpaperProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
+	}
+}
+
+func showWallpaper(
+	w app.Worker[*Provider],
+) app.WorkerFunc[app.ShowWallpaperProvider] {
+	return func(ctx app.Ctx, j app.Job[app.ShowWallpaperProvider]) error {
+		return w.Work(ctx, func(p *Provider) error { return j(p) })
 	}
 }
--- internal/config/provider.go
+++ internal/config/provider.go
@@ -6,6 +6,7 @@ type Provider struct {
 	store      domain.Store
 	tags       domain.TagRepo
 	wallpapers domain.WallpaperRepo
+	sources    domain.SourceRepo
 }
 
 func (p *Provider) Store() domain.Store {
@@ -18,4 +19,8 @@ func (p *Provider) TagRepo() domain.TagRepo {
 
 func (p *Provider) WallpaperRepo() domain.WallpaperRepo {
 	return p.wallpapers
+}
+
+func (p *Provider) SourceRepo() domain.SourceRepo {
+	return p.sources
 }
