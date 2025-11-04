79da490941b7fb7ac2ad4b36a80f1f7a97a9e73d
shimeoki <shimeoki@gmail.com>
2025-11-04 15:36:41 +0300

    style(app): provider keep-sorted

--- internal/app/provider.go
+++ internal/app/provider.go
@@ -7,14 +7,11 @@ import "github.com/shimeoki/wp/internal/domain"
 // to compose a specific provider for an action.
 type Provider any
 
-type WallpaperProvider interface {
-	Provider
-	WallpaperRepo() domain.WallpaperRepo
-}
+// keep-sorted start block=yes newline_separated=yes skip_lines=1
 
-type TagProvider interface {
+type AliasProvider interface {
 	Provider
-	TagRepo() domain.TagRepo
+	AliasRepo() domain.AliasRepo
 }
 
 type SourceProvider interface {
@@ -27,7 +24,14 @@ type StoreProvider interface {
 	Store() domain.Store
 }
 
-type AliasProvider interface {
+type TagProvider interface {
 	Provider
-	AliasRepo() domain.AliasRepo
+	TagRepo() domain.TagRepo
 }
+
+type WallpaperProvider interface {
+	Provider
+	WallpaperRepo() domain.WallpaperRepo
+}
+
+// keep-sorted end
